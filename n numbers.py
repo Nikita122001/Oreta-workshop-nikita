bc993161c87d994f18b8928f2f160469f7e6269e
Nikita <nikita@oreta.com.au>
2025-11-21 01:18:56 -0800

    n numbers

---
+++ n numbers.py
@@ -0,0 +1,9 @@
+try:
+    n = int(input("Enter a number: ") )
+
+    total = 0
+    for i in range(1, n + 1):
+        total += i
+    print(f"The sum of numbers from 1 to {n} is: {total}")
+except ValueError:
+    print("Invalid input")
